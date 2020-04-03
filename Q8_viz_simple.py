1d23d50b35093bb5a13f131d7b13c5fccd41ecba
d-giles <dgiles1@hawk.iit.edu>
2020-04-02 18:31:40 -0700

    Seems like simple example should work, but the Plots path is not working

--- Q8_viz_simple.py
+++ Q8_viz_simple.py
@@ -1,88 +1,16 @@
-import pickle
-import sys
-sys.path.append('python')
-import clusterOutliers
-import numpy as np
-from quarterTools import colors_for_plot, read_kepler_curve
 import pandas as pd
-from lightkurve import search_lightcurvefile
-from matplotlib.colors import to_hex
 
 from bokeh.io import curdoc
 from bokeh.plotting import figure
 from bokeh.layouts import row,gridplot,column
-from bokeh.models import ColumnDataSource, Circle
+from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Button, Select
 from bokeh.events import Tap
-from bokeh.palettes import Colorblind4,Category10
-from bokeh.transform import factor_cmap, linear_cmap, log_cmap
+from bokeh.palettes import Category10
 
-##### Data Import #####
+gui_df = pd.read_csv('gui_df.csv')
 
-with open('data/output/Q8_sample.coo','rb') as file:
-    Q8_coo = pickle.load(file)
-
-data               = Q8_coo.data
-Quarter            = 8
-pca                = Q8_coo.reductions['PCA90']
-x                  = pca.iloc[:,0]
-y                  = pca.iloc[:,1]
-files              = data.index
-labels             = np.array(['KIC '+str(int(lc[4:13])) for lc in files])
-dblabels           = Q8_coo.scores['DB_outliers']
-dbcolors           = colors_for_plot(dblabels,'color_blind')
-dblabels_as_string = np.array(dblabels,dtype=str)
-scores             = Q8_coo.scores['k1_1000x10']
-lin_colors         = colors_for_plot(-scores,'viridis')
-log_colors         = colors_for_plot(-np.log(scores),'viridis')
-png_dir           = 'Plots/'
-
-
-##### Bokeh Setup #####
-# Tools to use
-TOOLS     = "pan,wheel_zoom,reset,tap,box_select,poly_select"
-palette   = Colorblind4[0], Colorblind4[1], Colorblind4[3] # Color palette for outlier cmap
-alphas    = {1:1,2:.9,3:.8,4:.75,5:.7,6:.65,7:.6,8:.55,9:.5,10:.5}
-cmap_dict = {
-    "Base"            : palette[0],
-    "Outliers"        : [to_hex(c) for c in dbcolors],
-    "Scores - Linear" : [to_hex(c) for c in lin_colors],
-    "Scores - Log"    : [to_hex(c) for c in log_colors],
-}
-
-# Set up callbacks
-def update_plot():
-    try:
-        inds = s1.selected.indices
-        if len(inds)==0:
-            inds=[0]
-        if len(inds)>10:
-            inds = inds[:10] # too many lightcurves would be illegible, 10 is pushing it.
-    except:
-        inds = [0]
-    alpha = alphas[len(inds)]
-    s2.data = {'t':[],'nf':[]}
-    lcs = list(s1.data['desc'][inds])
-    lcfs = list(s1.data['files'][inds])
-    for i,ind in enumerate(inds):
-        # for local data
-        lcf = fits_dir+lcfs[i]
-        new_t, new_nf, _ = read_kepler_curve(lcf)
-        newdata = {'t':new_t,'nf':new_nf}
-        """
-        # for remote data
-        lc = lcs[i]
-        # download Kepler lighcurve via Lightkurve
-        lcf = search_lightcurvefile(lc, mission="Kepler", quarter=Quarter).download()
-        # use the normalized PDCSAP flux 
-        nlc = lcf.PDCSAP_FLUX.normalize()
-        newdata = {'t':nlc.time,'nf':nlc.flux}
-        """
-        s2.stream(newdata)
-        lg.glyph.line_alpha=alpha
-        #lg.glyph.line_color=palette[i]
-        #s2.data = {'t':t,'nf':nf}
-
+s1 = ColumnDataSource(gui_df)
 def update_colors(attrname, old, new):
     """ 
     Bokeh was being uncooperative with it's color mapper combined with the selection tool.
@@ -94,65 +22,19 @@ def update_colors(attrname, old, new):
     mapping as an array for the life of me. It's clunky and I hate it but it works.
     """
     if select.value == "Base":
-        s1.data['colors'] = [cmap_dict[select.value]]*len(s1.data['x'])
+        s1.data['colors'] = [Category10[10][0]]*len(s1.data['x'])
     else:
-        s1.data['colors'] = cmap_dict[select.value]
+        s1.data['colors'] = s1.data[select.value]
 
-# Bokeh data
-# create a column data source for the plots to share
-s1 = ColumnDataSource(data={
-    'x'     : x,
-    'y'     : y,
-    'desc'  : labels,
-    'files' : files,
-    'labels': dblabels_as_string,
-    'scores': scores,
-    'colors': [cmap_dict['Base']]*len(x) # See update_colors method for comments
-}) 
-
-s2=ColumnDataSource(data={
-    't'  : [],
-    'nf' : []
-})
-
-"""
-# Idea for multi-line plotting with different colors:
-ts = [[]]*10 # empty arrays for now 
-nfs = [[]]*10
-s2 = ColumnDataSource(data={
-    't':ts,
-    'nf':nfs,
-    'colors':Category10[10]
-})
-
-plc = figure(plot_width=1000,plot_height=200,title=None)
-lg = plc.multiline('t','nf',color='colors',source=s2)
 def update_plot():
     try:
-        inds = s1.selected.indices
-        if len(inds)==0:
-            inds=[0]
-        if len(inds)>10:
-            inds = inds[:10] # too many lightcurves would be illegible, 10 is pushing it.
+        ind = s1.selected.indices[0] # can only plot one at a time
     except:
-        inds = [0]
-    alpha = alphas[len(inds)]
-    lcs = list(s1.data['desc'][inds])
-    new_ts = ts
-    new_nfs = nfs
-    for i,ind in enumerate(inds):
-        lc = lcs[i]
+        ind = 0
 
-        # download Kepler lighcurve via Lightkurve
-        lcf = search_lightcurvefile(lc, mission="Kepler", quarter=Quarter).download()
-        # use the normalized PDCSAP flux 
-        nlc = lcf.PDCSAP_FLUX.normalize()
-        new_ts[i] = nlc.time
-        new_nfs[i] = nlc.flux
-    newdata = {'t':new_ts,'nf':new_nfs}
-    s2.stream(newdata)
-    lg.glyph.line_alpha=alpha
-"""
+    lc_path = s1.data['files'][ind]
+    lc_image = plc.image_url(url=[lc_path],x=0,y=0,w=10,h=3,anchor="bottom_left")
+
 # Set up widgets
 button = Button(label='Plot Selected')
 button.on_click(update_plot)
@@ -161,17 +43,26 @@ select = Select(title="Color Scheme:", value="Base", options=["Base", "Outliers"
 select.on_change('value',update_colors)
 
 # create a new plot and add a renderer
+TOOLS     = "pan,wheel_zoom,reset,tap,box_select,poly_select"
 left = figure(tools=TOOLS, plot_width=1000, plot_height=600, title=None)
+
 scatter = left.circle(x='x', y='y', fill_color='colors', line_color=None, size=4, source=s1)
-#left.on_event(Tap,update_plot)
-# Planning to incorporate a detailed view of the cluster center on a right plot in the future
-# create a plot for the light curve and add a line renderer
-plc = figure(tools=TOOLS,plot_width=1000,plot_height=200,title=None)
-lg = plc.line('t','nf',source=s2)
-update_plot()
+left.on_event(Tap,update_plot)
+plc = figure(plot_width=1000,plot_height=300,x_range=(0,10),y_range=(0,3))
+plc.xaxis.axis_line_color = None
+plc.yaxis.axis_line_color = None
+plc.xaxis.major_tick_line_color = None
+plc.xaxis.minor_tick_line_color = None
+plc.yaxis.major_tick_line_color = None
+plc.yaxis.minor_tick_line_color = None
+plc.xaxis.major_label_text_color = None
+plc.yaxis.major_label_text_color = None
+lc_path = s1.data['files'][0]
+lc_image = plc.image_url(url=[lc_path],x=0,y=0,w=10,h=3,anchor="bottom_left")
 
 # Set up layouts and add to document
 inputs = row(button,select)
 #p = gridplot([[left,right]]) #future planning
 layout = column(inputs, left, plc)
 curdoc().add_root(layout)
+    
